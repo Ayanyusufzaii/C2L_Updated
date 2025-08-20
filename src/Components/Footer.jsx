23626323b2fe3ecaf0321811402635afd452a04c
shubhamkzn <shubhamkaizen34@gmail.com>
2025-08-20 16:56:03 +0530

    ftr

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,19 +11,19 @@ import tiktok from "../assets/tiktokicon.png";
 import fb from "../assets/fbicon.png";
 import x from "../assets/xicon.png";
 import addressicon from "../assets/addressicon.png";
-
+ 
 function Footer() {
   const handleCall = () => {
     window.location.href = "tel:+61470695167";
   };
-
+ 
   const handleEmail = () => {
     window.location.href = "mailto:teamup@connect2lawyer.com.au";
   };
-
+ 
   return (
     <div className="bg-[#023437] text-white w-full">
-      <div className="max-w-7xl mx-auto pt-8 md:pt-16 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto pt-8 md:pt-16 px-2 md:px-6 lg:px-8">
         {/* Desktop and Tablet Layout - hidden on screens smaller than md */}
         <div className="hidden md:block lg:ml-[-3%]">
           <div className="flex flex-col md:flex-row justify-between items-start border-b border-teal-800 pb-4 px-4">
@@ -52,7 +52,7 @@ function Footer() {
                     onClick={handleEmail}
                   />
                   <span
-                    className="text-sm sm:text-base md:text-lg lg:text-[20px] font-semibold "
+                    className="text-sm sm:text-base md:text-lg lg:text-[20px] font-semibold"
                     onClick={handleEmail}
                   >
                     teamup@connect2lawyer.com.au
@@ -96,7 +96,7 @@ function Footer() {
                   />
                 </a>
               </div>
-
+ 
               {/* LinkedIn */}
               <div className="flex items-center justify-center">
                 <a
@@ -112,7 +112,7 @@ function Footer() {
                   />
                 </a>
               </div>
-
+ 
               {/* X (Twitter) */}
               <div className="flex items-center justify-center">
                 <a
@@ -124,7 +124,7 @@ function Footer() {
                   <img src={x} alt="X" className="w-[42px] h-[42px]" />
                 </a>
               </div>
-
+ 
               {/* Second row */}
               {/* Facebook */}
               <div className="flex items-center justify-center">
@@ -137,7 +137,7 @@ function Footer() {
                   <img src={fb} alt="Facebook" className="w-[42px] h-[42px]" />
                 </a>
               </div>
-
+ 
               {/* Instagram */}
               <div className="flex items-center justify-center">
                 <a
@@ -153,7 +153,7 @@ function Footer() {
                   />
                 </a>
               </div>
-
+ 
               {/* YouTube */}
               <div className="flex items-center justify-center">
                 <a
@@ -167,7 +167,7 @@ function Footer() {
               </div>
             </div>
           </div>
-
+ 
           {/* Connect2Lawyer Title for Desktop/Tablet */}
           <div className="py-6 lg:py-8 w-full flex justify-center lg:pl-4 xl:pl-10">
             {/* For 4K/laptop/tab: show a full-width image, for mobile: keep previous layout */}
@@ -180,7 +180,7 @@ function Footer() {
             </div>
           </div>
         </div>
-
+ 
         {/* Mobile Layout - shown on screens smaller than md */}
         <div className="md:hidden block overflow-x-hidden">
           <div>
@@ -192,38 +192,59 @@ function Footer() {
                 className="w-full h-auto object-contain pb-4"
               />
             </div>
-
+ 
             {/* Contact info for Mobile */}
             <div className="flex flex-col space-y-4 ">
               <div className="flex items-center font-opensans">
                 <img
-                  className=" w-14 h-14"
+                  className="
+      w-[48px] h-[48px]
+      min-[375px]:w-[60px] min-[375px]:h-[60px]
+      min-[425px]:w-[60px] min-[425px]:h-[60px]
+      sm:w-[60px] sm:h-[60px]
+      mr-2"
                   src={dialer}
                   alt="Phone Icon"
                   onClick={handleCall}
                 />
                 <span
-                  className="text-lg sm:text-xl font-semibold "
+                  className="
+      text-[15px]
+      min-[375px]:text-[16px]
+      min-[425px]:text-[18px]
+      sm:text-[16px]
+      font-semibold"
                   onClick={handleCall}
                 >
                   +61 470 695 167
                 </span>
               </div>
-
+ 
               <div className="flex items-center font-opensans">
                 <img
-                  className=" w-14 h-14"
+                  className="
+      w-[48px] h-[48px]
+      min-[375px]:w-[60px] min-[375px]:h-[60px]
+      min-[425px]:w-[60px] min-[425px]:h-[60px]
+      sm:w-[60px] sm:h-[60px]
+      mr-2"
                   src={sms}
                   alt="Mail Icon"
                   onClick={handleEmail}
                 />
                 <span
-                  className="text-lg sm:text-base font-semibold break-words "
+                  className="
+      text-[15px]
+      min-[375px]:text-[16px]
+      min-[425px]:text-[18px]
+      sm:text-[16px]
+      font-semibold"
                   onClick={handleEmail}
                 >
                   teamup@connect2lawyer.com.au
                 </span>
               </div>
+ 
               <div className="flex items-center font-opensans">
                 <a
                   href="https://www.google.com/maps/place/45+Monterey+Bay+Drive,+Point+Cook+VIC+3030,+Australia"
@@ -232,17 +253,27 @@ function Footer() {
                   className="flex items-center"
                 >
                   <img
-                    className="w-14 h-14 mr-2"
+                    className="
+        w-[48px] h-[48px]
+      min-[375px]:w-[60px] min-[375px]:h-[60px]
+      min-[425px]:w-[60px] min-[425px]:h-[60px]
+      sm:w-[60px] sm:h-[60px]
+        mr-2"
                     src={addressicon}
                     alt="Address Icon"
                   />
-                  <span className="text-lg sm:text-base font-semibold hover:underline break-words">
+                  <span className="
+      text-[15px]
+      min-[375px]:text-[16px]
+      min-[425px]:text-[18px]
+      sm:text-[18px]
+      font-semibold hover:underline break-words">
                     45 Monterey Bay Drive, Point Cook 3030, Melbourne, Victoria,
                     Australia
                   </span>
                 </a>
               </div>
-
+ 
               {/* Social Media Icons - Horizontal Layout, Center Aligned */}
               <div className="flex justify-center items-center px-2 pt-2">
                 <div className="flex justify-between items-center w-full gap-2">
@@ -320,7 +351,7 @@ function Footer() {
                   </a>
                 </div>
               </div>
-
+ 
               {/* Privacy Policy and Disclaimer - Below Social Icons */}
               <div className="flex justify-center gap-4 text-xs text-gray-300 pt-4 font-opensans">
                 <a href="Privacy-Policy" className="hover:underline">
@@ -330,12 +361,12 @@ function Footer() {
                   Disclaimer
                 </a>
               </div>
-
+ 
               {/* Single Divider Line - Aligned with Social Icons */}
               <div className="flex justify-center px-4">
                 <div className="w-full max-w-sm border-t border-teal-800"></div>
               </div>
-
+ 
               {/* Copyright Text - At Bottom */}
               <div className="text-center text-xs text-gray-300 pt-2 font-opensans">
                 Copyright ©2025 C2L All Rights Reserved
@@ -344,7 +375,7 @@ function Footer() {
           </div>
         </div>
       </div>
-
+ 
       {/* Footer Bottom Bar - Only for Desktop */}
       <div className="hidden md:block border-t border-teal-800 mt-8 pt-4 pb-4 px-2 sm:px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 font-opensans">
         <div className="flex flex-col md:flex-row items-center justify-between text-[10px] sm:text-xs md:text-sm lg:text-base xl:text-lg 2xl:text-xl text-gray-300 w-full max-w-full">
@@ -367,5 +398,7 @@ function Footer() {
     </div>
   );
 }
-
+ 
 export default Footer;
+
+ 
